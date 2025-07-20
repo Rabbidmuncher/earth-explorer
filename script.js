a1ad9d6c04f03748441c826273a4978ecdda2fed
Rabbidmuncher <noky@tvnet.lv>
2025-07-20 23:06:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,4 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-// Add click handler to update Street View iframe
-map.on('click', function(e) {
-    var lat = e.latlng.lat;
-    var lng = e.latlng.lng;
-    var iframe = document.getElementById('street-view');
-    var embedUrl = `https://www.google.com/maps/embed/v1/streetview?key=AIzaSyB2CHeF6LOtV573Aef65UJJnySN4_g0iYk&location=${lat},${lng}&heading=210&pitch=10&fov=75`;
-    iframe.src = embedUrl;
 });
