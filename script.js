b531d7e19bdd09282d71a97f37610f3bf39cdeb0
Rabbidmuncher <noky@tvnet.lv>
2025-07-20 23:11:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,3 @@ var map = L.map('map').setView([20, 0], 2);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
-
-});
